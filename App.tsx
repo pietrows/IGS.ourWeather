c9c92fddb99eeadd452c011b8e3a01567e646ecc
Vitor <vitorsilvestredev@outlook.com>
2021-11-16 20:41:50 -0300

    first commit

--- App.tsx
+++ App.tsx
@@ -1,10 +1,16 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { SafeAreaProvider } from "react-native-safe-area-context";
 
-import useCachedResources from './hooks/useCachedResources';
-import useColorScheme from './hooks/useColorScheme';
-import Navigation from './navigation';
+import useCachedResources from "./hooks/useCachedResources";
+import useColorScheme from "./hooks/useColorScheme";
+import Navigation from "./navigation";
+import { CurrentTemperature } from "./components/CurrentTemperature";
+import { OHA } from "./components/OHA";
+
+// icone
+import { EvilIcons } from "@expo/vector-icons";
+import { StyleSheet, Text, View, TouchableOpacity } from "react-native";
 
 export default function App() {
   const isLoadingComplete = useCachedResources();
@@ -15,9 +21,22 @@ export default function App() {
   } else {
     return (
       <SafeAreaProvider>
-        <Navigation colorScheme={colorScheme} />
-        <StatusBar />
+        <TouchableOpacity style={stylesTest.refreshButton}>
+          <EvilIcons name="refresh" size={45} />
+        </TouchableOpacity>
+        <CurrentTemperature />
+        <OHA />
+        {/* <Navigation colorScheme={colorScheme} />
+        <StatusBar /> */}
       </SafeAreaProvider>
     );
   }
 }
+
+const stylesTest = StyleSheet.create({
+  refreshButton: {
+    position: "absolute",
+    alignSelf: "flex-start",
+    margin: 30,
+  },
+});
